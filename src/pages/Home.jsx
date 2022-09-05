124a8caa2102a7c357673c9ce4a6a9b5d4819fb4
ramadlana <hidayahweb@gmail.com>
2022-09-05 11:54:12 +0700

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,16 +8,15 @@ export default function Homepage() {
     <div>
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-          <a
-            href="/"
+          <p
             className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             role="alert"
           >
             <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
-              Hello
+              Hello There
             </span>
 
-            <span className="text-sm font-medium">Hidayah Ramadlana</span>
+            <span className="text-sm font-medium">Good to see you</span>
             <svg
               className="ml-2 w-5 h-5"
               fill="currentColor"
@@ -30,7 +29,7 @@ export default function Homepage() {
                 clipRule="evenodd"
               />
             </svg>
-          </a>
+          </p>
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
             Rama React 18 Custom Starterkit
           </h1>
@@ -60,14 +59,6 @@ export default function Homepage() {
               onClick={() => navigate("/example/menugroup")}
               className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
-              <svg
-                className="mr-2 -ml-1 w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
-              </svg>
               Tab Menus
             </button>
           </div>
@@ -76,10 +67,7 @@ export default function Homepage() {
               Find me On
             </span>
             <div className="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
-              <a
-                href="/"
-                className="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400"
-              >
+              <p className="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
                 <svg
                   className="h-8"
                   viewBox="0 0 132 29"
@@ -123,11 +111,8 @@ export default function Homepage() {
                     fill="currentColor"
                   />
                 </svg>
-              </a>
-              <a
-                href="/"
-                className="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400"
-              >
+              </p>
+              <p className="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
                 <svg
                   className="h-11"
                   viewBox="0 0 208 42"
@@ -147,11 +132,8 @@ export default function Homepage() {
                     fill="currentColor"
                   />
                 </svg>
-              </a>
-              <a
-                href="/"
-                className="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400"
-              >
+              </p>
+              <p className="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
                 <svg
                   className="h-11"
                   viewBox="0 0 120 41"
@@ -175,7 +157,7 @@ export default function Homepage() {
                     fill="currentColor"
                   />
                 </svg>
-              </a>
+              </p>
             </div>
           </div>
         </div>
@@ -200,12 +182,12 @@ export default function Homepage() {
           <div className="grid grid-cols-2 gap-4 mt-8">
             <img
               className="w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+              src="/office-long-2.png"
               alt="office content 1"
             />
             <img
               className="mt-4 w-full lg:mt-10 rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+              src="/office-long-1.png"
               alt="office content 2"
             />
           </div>
